bbe668699c4be8e2e7f02a2ee1e12ec387fcd849
SAYDUR <saydur@labs-mac.local>
2020-01-16 16:51:37 +0600

    Showing DATA by API

--- API_callByCocoapod/ViewController.swift
+++ API_callByCocoapod/ViewController.swift
@@ -13,6 +13,20 @@ import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text_view: UITextView!
+    @IBAction func callButton(_ sender: Any) {
+        
+        //Function Call for respose API & print them in log
+        
+        Alamofire.request("https://api.darksky.net/forecast/688555c90fac582a0fbc3be8e444802b/37.8267,-122.4233").responseJSON{ response in
+            print(response.request as Any)
+            print(response.response as Any)
+            print(response.result.value as Any)
+            
+            // Code for showing data in text view
+            self.text_view.text = response.description
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
