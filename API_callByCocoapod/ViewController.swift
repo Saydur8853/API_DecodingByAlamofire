71f5da85639a89889b98620c728aa799578ac958
SAYDUR <saydur@labs-mac.local>
2020-01-22 17:38:59 +0600

    DecodedByAlamofire

--- API_callByCocoapod/ViewController.swift
+++ API_callByCocoapod/ViewController.swift
@@ -9,29 +9,65 @@
 import UIKit
 import Alamofire
 
+struct Course :Decodable {
+    let id: Int?
+    let name: String?
+    let link: String?
+    let imageUrl: String?
+    let number_of_lessons: Int?
+}
 
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var text_view: UITextView!
-    @IBAction func callButton(_ sender: Any) {
-        
-        //Function Call for respose API & print them in log
-        
-        Alamofire.request("https://api.darksky.net/forecast/688555c90fac582a0fbc3be8e444802b/37.8267,-122.4233").responseJSON{ response in
-            print(response.request as Any)
-            print(response.response as Any)
-            print(response.result.value as Any)
-            
-            // Code for showing data in text view
-            self.text_view.text = response.description
-        }
-    }
+    
+        let decoder = JSONDecoder()
+    var courseString: String = ""
+    
+//    @IBAction func callButton(_ sender: Any) {
+//
+//        //Function Call for respose API & print them in log
+//
+//        let url = "https://restcountries.eu/rest/v2/all"
+//        let urlObj = URL(string: url)
+//
+//    }
+//
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
+    @IBAction func callButton(_ sender: Any) {
+        
+        
+        Alamofire.request("https://api.letsbuildthatapp.com/jsondecodable/courses" ).validate().responseJSON { response in
+            print(response.debugDescription)
+            
+            do{
+                
+                let courses = try self.decoder.decode([Course].self, from: response.data! )
+                
+                for item in courses {
+                    print(item.name!)
+                    
+                    
+                    //self.text_view.text = item.name
+                    self.courseString += item.name! + "\n"
+                    
+                }
+                
+                self.text_view.text = self.courseString
+                
+            } catch{
+                
+                print("Error")
+            }
+        }
+    }
 
 }
 
